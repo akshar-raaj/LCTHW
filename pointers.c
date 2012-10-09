0036b69ad650083f0bc5956a27a924fc3ce4fc4a
Akshar Raaj <akshar@agiliq.com>
2012-10-09 20:01:49 +0530

    accessing array with pointer

--- pointers.c
+++ pointers.c
@@ -13,5 +13,20 @@ int main(int argc, char *argv[])
     ptr = &b;
     *ptr = 70;
     printf("b is %d\n", b);
+
+    int my_array[] = {1, 5, 9, -6, 10};
+    int *arr_ptr = &my_array[0];
+    int i = 0;
+    for(i=0; i<5; i++)
+    {
+        printf("Number %d is %d\n", i, *(arr_ptr+i));
+    }
+
+    int *arr_ptr2 = my_array; //c standards say &my_array[0] is equivalent to my_array
+    int j = 0;
+    for(j=0; j<5; j++)
+    {
+        printf("Number %d is %d\n", j, *arr_ptr2++);
+    }
     return 0;
 }
