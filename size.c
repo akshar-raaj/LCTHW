172b083716b017af0e64e37d12ef6b0b391db4cd
Akshar Raaj <akshar@agiliq.com>
2012-10-09 22:30:28 +0530

    Size of different types

---
+++ size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Size of short is %d\n", sizeof(short));
+    printf("Size of int is %d\n", sizeof(int));
+    printf("Size of long is %d\n", sizeof(long));
+    printf("Size of char is %d\n", sizeof(char));
+    return 0;
+}
