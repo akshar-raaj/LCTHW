a30101fe4396173d83f9254699141212d73bc5d5
Akshar Raaj <akshar@agiliq.com>
2012-10-10 21:43:02 +0530

    Implement strchr using pointers

--- pointers_string.c
+++ pointers_string.c
@@ -26,6 +26,23 @@ void strcat_impl(char *str1, char *str2)
     *ptr = '\0';
     printf("concatenated string is %s\n", arr);
 }
+
+int strchr_impl(char *str, char c)
+{
+    int i = 0;
+    int is_found = -1;
+    while(*str != '\0')
+    {
+        if(*str == c)
+        {
+            is_found = i;
+            break;
+        }
+        str++;
+        i++;
+    }
+    return is_found;
+}
 int main(int argc, char *argv[])
 {
     int len = strlen_impl("abc");
@@ -34,6 +51,13 @@ int main(int argc, char *argv[])
     printf("Length of akshar is %d\n", len);
     len = strlen_impl("");
     printf("Length of is %d\n", len);
+
     strcat_impl("akshar", "raaj");
+
+    int found = strchr_impl("akshar", 's');
+    printf("Found at %d\n", found);
+    found = strchr_impl("akshar", 'b');
+    printf("Found at %d\n", found);
+
     return 0;
 }
