36d2b98fd8df1b03e4a8a100c7c1653a21e73285
Akshar Raaj <akshar@agiliq.com>
2012-10-10 11:46:56 +0530

    pointers from lcthw

--- ex15.c
+++ ex15.c
@@ -11,6 +11,17 @@ int main(int argc, char *argv[])
     printf("First character is %c\n", name[0]);
     printf("Second character is %c\n", name[1]);
 
+    char another[] = {'s', 'h', 'l', 'o', 'k', 'a', '\0'};
+    printf("Another is %s\n", another);
+
+    char *an_p = another;
+
+    printf("First character is %c\n", *an_p);
+    printf("Second character is %c\n", *(an_p+1));
+
+    printf("First character is %c\n", an_p[0]);
+    printf("Second character is %c\n", an_p[1]);
+
     int nums[] = {12, 34, 5, 67, 81};
     int i;
 
@@ -27,6 +38,17 @@ int main(int argc, char *argv[])
 
     printf("First number is %d\n", num_p[0]);
     printf("Second number is %d\n", num_p[1]);
+
+    *(num_p+1) = *(num_p+1) + 5;
+    printf("Second number is %d\n", num_p[1]);
+
+    char *names[] = {"akshar", "shabda", "shloka"};
+
+    printf("First name is %s\n", names[0]);
+    printf("Second name is %s\n", names[1]);
+
+    printf("First name is %s\n", *names);
+    printf("Second name is %s\n", *(names+1));
 
     return 0;
 }
