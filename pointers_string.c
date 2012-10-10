f8dad22634c5950a85e7b8b3375bd78f1288cce0
Akshar Raaj <akshar@agiliq.com>
2012-10-10 20:52:55 +0530

    Implement strlen and strcat using pointers

---
+++ pointers_string.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int strlen_impl(char *str)
+{
+    int i = 0;
+    while(*str != '\0')
+    {
+        i++;
+        str++;
+    }
+    return i;
+}
+
+void strcat_impl(char *str1, char *str2)
+{
+    char arr[40];
+    char *ptr = arr;
+    while(*str1 != '\0')
+    {
+        *ptr = *str1;
+        str1++;
+        ptr++;
+    }
+    while(*str2 != '\0')
+    {
+        *ptr = *str2;
+        str2++;
+        ptr++;
+    }
+    *ptr = '\0';
+    printf("concatenated string is %s\n", arr);
+}
+int main(int argc, char *argv[])
+{
+    int len = strlen_impl("abc");
+    printf("Length of abc is %d\n", len);
+    len = strlen_impl("akshar");
+    printf("Length of akshar is %d\n", len);
+    len = strlen_impl("");
+    printf("Length of is %d\n", len);
+
+    strcat_impl("akshar", "raaj");
+    return 0;
+}
