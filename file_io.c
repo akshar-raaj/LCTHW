72b8e00740683ebfabdf3676843eac05ed07f592
Akshar Raaj <akshar@agiliq.com>
2012-10-20 17:18:21 +0530

    Write a struct to file and then read it from file

--- file_io.c
+++ file_io.c
@@ -2,11 +2,40 @@
 #include <stdlib.h>
 #include <assert.h>
 
+struct Person
+{
+    int age;
+    char *name;
+};
+
+struct Person *Person_create(int age, char *name)
+{
+    struct Person *person = malloc(sizeof(struct Person));
+    assert(person!=NULL);
+    person->age = age;
+    person->name = name;
+    return person;
+}
+
+struct Person *Person_create_empty()
+{
+    struct Person *person = malloc(sizeof(struct Person));
+    assert(person!=NULL);
+    return person;
+}
+
+void Person_print(struct Person *person)
+{
+    printf("Name is %s\n", person->name);
+    printf("Age is %d\n", person->age);
+}
+
 void die(char *message)
 {
-    printf(message);
+    printf("%s", message);
     exit(1);
 }
+
 int main(int argc, char *argv[])
 {
     if (argc<2)
@@ -16,10 +45,20 @@ int main(int argc, char *argv[])
 
     char *filename = argv[1];
     FILE *fp;
-    fp = fopen(filename, "w");
+    char *mode = "w";
+    fp = fopen(filename, mode);
     assert(fp!=NULL);
 
-    fprintf(fp, "Testing\n");
+    //fprintf(fp, "Testing....\n");
+    struct Person *person = Person_create(11, "test");
+
+    int ret = fwrite(person, sizeof(struct Person), 1, fp);
+    if(ret != 1)
+    {
+        die("Could not write struct to file\n");
+    }
+
+    free(person);
 
     int closed = fclose(fp);
     if(closed != 0)
@@ -27,5 +66,15 @@ int main(int argc, char *argv[])
         die("File could not be closed properly");
     }
 
+    fp = fopen(filename, "r+");
+    assert(fp!=NULL);
+    person = Person_create_empty();
+    ret = fread(person, sizeof(struct Person), 1, fp);
+    if(ret != 1)
+    {
+        die("Could not read from the file\n");
+    }
+
+    Person_print(person);
     return 0;
 }
