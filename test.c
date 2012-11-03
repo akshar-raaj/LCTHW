edd4a68ef55524f736916e619ef564b1a11d9f82
Akshar Raaj <akshar@agiliq.com>
2012-11-03 11:54:09 +0530

    function pointer

--- test.c
+++ test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+typedef void (*increase)(int number);
+
+void increase_1(int number)
+{
+    printf("Number is %d\n", (number+1));
+}
+
+void increase_2(int number)
+{
+    printf("Number is %d\n", (number+2));
+}
+
+void use_increase(int number, increase fun)
+{
+    fun(number);
+}
+
 int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
+    /*printf("Hello World!\n");
     printf("Number of arguments is %d\n", argc);
 
     int i =0;
@@ -18,7 +35,16 @@ int main(int argc, char *argv[])
     printf("address first argument is %p\n", addr);
     printf("first argument is %s\n", addr);
     printf("first character of first argument is %c\n", *addr);
-    printf("address first argument is %p\n", &argv[0]);
+    printf("address first argument is %p\n", &argv[0]);*/
+
+    /*int a = 10;
+    int *ptr = &a;
+    printf("a is %d\n", a);
+    *ptr = 20;
+    printf("a is %d\n", a);*/
+
+    use_increase(5, increase_1);
+    use_increase(5, increase_2);
 
     return 0;
 }
